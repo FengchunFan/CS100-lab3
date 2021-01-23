13195c6d5813fe0ba5f7230becf1503365c8bf6f
danams101 <danams101@yahoo.com>
2021-01-23 13:12:58 -0800

    Add completed Op class and unit tests
    * Completed Op class implementation
    * Added more unit tests to op_test.hpp

--- op_test.hpp
+++ op_test.hpp
@@ -8,6 +8,27 @@
 TEST(OpTest, OpEvaluateNonZero) {
     Op* test = new Op(8);
     EXPECT_EQ(test->evaluate(), 8);
+    EXPECT_EQ(test->stringify(), "8.000000");
 }
+
+TEST(OpTest, OpEvaluateNegative) {
+    Op* test = new Op(-8.5);
+    EXPECT_EQ(test->evaluate(), -8.5);
+    EXPECT_EQ(test->stringify(), "-8.500000");
+}
+
+TEST(OpTest, OpEvaluateZero) {
+    Op* test = new Op(0);
+    EXPECT_EQ(test->evaluate(), 0);
+    EXPECT_EQ(test->stringify(), "0.000000");
+}
+
+TEST(OpTest, OpEvaluateLargeNumber) {
+    Op* test = new Op(10000);
+    EXPECT_EQ(test->evaluate(), 10000);
+    EXPECT_EQ(test->stringify(), "10000.000000");
+}
+
+
 
 #endif //__OP_TEST_HPP__
