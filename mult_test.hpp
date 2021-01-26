8299616b6d2d2fbc7369440a1d5dd625cf3bada9
danams101 <danams101@yahoo.com>
2021-01-25 20:59:38 -0800

    Added more Mult class tests
    * added more tests to mult_test.hpp
    * added some mock classes for some tests to mock_tests.hpp

--- mult_test.hpp
+++ mult_test.hpp
@@ -3,6 +3,7 @@
 
 #include "gtest/gtest.h"
 
+#include "mock_tests.hpp"
 #include "mult.hpp"
 #include "op.hpp"
 
@@ -10,6 +11,42 @@ TEST(MultTest, MultEvaluateNonZero) {
     Mult* test = new Mult(new Op(8), new Op(10));
     EXPECT_EQ(test->evaluate(), 80);
     EXPECT_EQ(test->stringify(), "(8.000000*10.000000)"); 
+}
+
+TEST(MultTest, MultEvaluateZero) {
+    Mult* test = new Mult(new Op(0), new Op(5));
+    EXPECT_EQ(test->evaluate(), 0);
+    EXPECT_EQ(test->stringify(), "(0.000000*5.000000)");
+}
+
+TEST(MultTest, MultEvaluateOneNegative) {
+    Mult* test = new Mult(new Op(-1), new Op(5));
+    EXPECT_EQ(test->evaluate(), -5);
+    EXPECT_EQ(test->stringify(), "(-1.000000*5.000000)");
+}
+
+TEST(MultTest, MultEvaluateBothNegative) {
+    Mult* test = new Mult(new Op(-1), new Op(-5));
+    EXPECT_EQ(test->evaluate(), 5);
+    EXPECT_EQ(test->stringify(), "(-1.000000*-5.000000)");
+}
+
+TEST(MultTest, MultEvaluateLargeNumber) {
+    Mult* test = new Mult(new Op(1000), new Op(1000));
+    EXPECT_EQ(test->evaluate(), 1000000);
+    EXPECT_EQ(test->stringify(), "(1000.000000*1000.000000)");
+}
+
+TEST(MultTest, MultEvaluateSmallNumber) {
+    Mult* test = new Mult(new Op(0.001), new Op(0.001));
+    EXPECT_EQ(test->evaluate(), 0.000001);
+    EXPECT_EQ(test->stringify(), "(0.001000*0.001000)");
+}
+
+TEST(MultTest, MultWithMockFunctions) {
+    Mult* test = new Mult(new DivByNonZeroMock(), new AddMock());
+    EXPECT_EQ(test->evaluate(), 60);
+    EXPECT_EQ(test->stringify(), "((10.000000/2.000000)*(10.000000+2.000000))");
 }
 
 #endif // __MULT_TEST_HPP__
