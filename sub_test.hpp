0bceb252432e18ad9a364667fa891a802460194b
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-01-27 10:04:32 -0800

    Fixed mistakes in sub_test

--- sub_test.hpp
+++ sub_test.hpp
@@ -5,13 +5,14 @@
 
 #include "sub.hpp"
 #include "op.hpp"
+#include "base.hpp"
 
 TEST(SubTest, SubtractionBetweenZeros) {
 	 Base* val = new Op(0);
 
 	 Base* output = new SUB(val, val);
-	 EXPECT_EQ(sub->evaluate(), 0);
-     	 EXPECT_EQ(sub->stringify(), "(0.000000 - 0.000000)");
+	 EXPECT_EQ(output->evaluate(), 0);
+     	 EXPECT_EQ(output->stringify(), "(0.000000 - 0.000000)");
 }
 
 TEST(SubTest, SubtractionBetweenPositive) {
@@ -19,8 +20,8 @@ TEST(SubTest, SubtractionBetweenPositive) {
 	Base* val2 = new Op(4);
 	Base* output = new SUB(val1, val2);
     
-	EXPECT_EQ(sub->evaluate(), 1);
-    	EXPECT_EQ(sub->stringify(), "(5.000000 - 4.000000)");
+	EXPECT_EQ(output->evaluate(), 1);
+    	EXPECT_EQ(output->stringify(), "(5.000000 - 4.000000)");
 }
 
 TEST(SubTest, SubtractionBetweenNegative) {
@@ -28,8 +29,8 @@ TEST(SubTest, SubtractionBetweenNegative) {
         Base* val2 = new Op(-2);
         Base* output = new SUB(val1, val2);
 
-        EXPECT_EQ(sub->evaluate(), 1);
-        EXPECT_EQ(sub->stringify(), "(-1.000000 - -2.000000)");
+        EXPECT_EQ(output->evaluate(), 1);
+        EXPECT_EQ(output->stringify(), "(-1.000000 - -2.000000)");
 }
 
 TEST(SubTest, SubtractionBetweenLarge) {
@@ -37,8 +38,8 @@ TEST(SubTest, SubtractionBetweenLarge) {
         Base* val2 = new Op(1000);
         Base* output = new SUB(val1, val2);
 
-        EXPECT_EQ(sub->evaluate(), 1000);
-        EXPECT_EQ(sub->stringify(), "(2000.000000 - 1000.000000)");
+        EXPECT_EQ(output->evaluate(), 1000);
+        EXPECT_EQ(output->stringify(), "(2000.000000 - 1000.000000)");
 }
 
 TEST(SubTest, SubtractionBetweenSmall) {
@@ -46,8 +47,8 @@ TEST(SubTest, SubtractionBetweenSmall) {
         Base* val2 = new Op(0.000001);
         Base* output = new SUB(val1, val2);
 
-        EXPECT_EQ(sub->evaluate(), 0.000001);
-        EXPECT_EQ(sub->stringify(), "(0.000002 - 0.000001)");
+        EXPECT_EQ(output->evaluate(), 0.000001);
+        EXPECT_EQ(output->stringify(), "(0.000002 - 0.000001)");
 }
 
 #endif
